051209d7c61d42f23e04043219d0a37d27bd6dba
SergioAlias <sergio08071998@gmail.com>
2024-02-14 13:01:25 +0100

    created R script for number of HPO terms per tissue

---
+++ tissue_nHPO.R
@@ -0,0 +1,17 @@
+#!/usr/bin/env Rscript
+
+# Sergio Alías, 20240214
+# Last modified 20240214
+
+# tissue_nHPO.R
+
+# Plots for the number of HPO terms per tissue
+
+
+# Libs
+
+library(data.table)
+library(ggplot2)
+
+
+# Main script
