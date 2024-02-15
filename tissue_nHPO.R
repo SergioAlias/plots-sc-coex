1201126ed239a17193761694c40c7503dbc4d5a4
SergioAlias <sergio08071998@gmail.com>
2024-02-15 13:25:24 +0100

    working first version

--- tissue_nHPO.R
+++ tissue_nHPO.R
@@ -10,8 +10,47 @@
 
 # Libs
 
-library(data.table)
 library(ggplot2)
 
 
 # Main script
+
+data <- table(read.table("data/coment_per_HPO.tsv",
+                        header = TRUE,
+                        sep = "\t",
+                        colClasses = "character")[[1]])
+
+df <- as.data.frame(data)
+colnames(df) <- c("Tissue", "nHPOterms")
+
+## Eliminar tissues no analizados
+
+tissues_to_remove <- c("lymph-node",
+                        "rectum",
+                        "placenta",
+                        "endometrium")
+
+df <- subset(df, !grepl(paste(tissues_to_remove, collapse="|"), Tissue))
+
+## Cambiar guiones por espacios y primera letra en mayúsculas
+
+df$Tissue <- sapply(strsplit(as.character(df$Tissue), "-"), function(x) {
+    paste(toupper(substring(x, 1, 1)), substring(x, 2), sep = "", collapse = " ")
+})
+
+## Ordering
+
+df <- df[order(df$nHPOterms), ]
+df$Tissue <- factor(df$Tissue, levels = df$Tissue)
+
+## Plotting
+
+ggplot(data = df, aes(x = Tissue, y = nHPOterms, fill = nHPOterms)) +
+    geom_bar(stat = "identity", position = position_dodge())+
+    geom_text(aes(label = nHPOterms), hjust = 1.6, color = "white",
+            size=3.5) +
+    scale_fill_gradient(low = "green", high = "darkgreen") +
+    labs(x = element_blank(), y = "Number of HPO terms") +
+    coord_flip() +
+    theme_minimal() +
+    theme(legend.title = element_blank())
