5cb37a93a536d8a64ea132ad8f123a8bb5f73eaa
SergioAlias <sergio08071998@gmail.com>
2024-02-26 13:53:50 +0100

    created scatterplot script

---
+++ fc-coex-scatterplot.R
@@ -0,0 +1,18 @@
+#!/usr/bin/env Rscript
+
+# Sergio Alías, 20240226
+# Last modified 20240226
+
+# fc-coex-scatterplot.R
+
+# Plots p-value for fc and coex in a scatterplot
+
+
+# Libs
+
+library(ggplot2)
+
+
+# Main script
+
+## load fc values
