c166f60b467ab2961f9cbc6ccd5c72e324d12037
SergioAlias <sergio08071998@gmail.com>
2024-03-05 16:33:48 +0100

    applied logs

--- fc-coex-scatterplot.R
+++ fc-coex-scatterplot.R
@@ -18,19 +18,24 @@ library(ggpubr)
 
 ## load file
 
-dt <- fread("/home/sergio/projects/plots-sc-coex/outs/solo_comunes_pvals_FDR.tsv")
+data <- fread("/home/sergio/projects/plots-sc-coex/outs/solo_comunes_pvals_FDR.tsv")
+
+dt <- data
+
+dt$coex_FDR <- -log10(dt$coex_FDR)
+dt$coex_FDR <- -log10(dt$fc_FDR)
 
 pdf("outs/fc-coex-scatterplot.pdf")
 
 ggscatter(dt, x = "coex_FDR", y = "fc_FDR",
-          xlab = "COEX adjusted p-value",
-          ylab = "Fold change adjusted p-value",
+          xlab = "-log10 COEX adjusted p-value",
+          ylab = "-log10 Fold change adjusted p-value",
           color = "black", shape = 21, size = 3,
           add = "reg.line",
           add.params = list(color = "darkgreen", fill = "lightgray"),
           conf.int = TRUE,
           cor.coef = TRUE,
-          cor.coeff.args = list(method = "pearson", label.x = 0.7, label.y = 0.6, label.sep = "\n", geom = "label")
+          cor.coeff.args = list(method = "pearson", label.sep = "\n", geom = "label")
 )
 
 dev.off()
